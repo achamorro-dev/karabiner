daab56e34aca7c0bc7f160fa55719089b93c3394
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-08-29 13:21:45 +0200

    feat: add custom karabiner rules

--- rules.ts
+++ rules.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import { KarabinerRules } from "./types";
-import { createHyperSubLayers, app, open } from "./utils";
+import { app, command, createHyperSubLayers, open } from "./utils";
 
 const rules: KarabinerRules[] = [
   // Define the Hyper key itself
@@ -21,6 +21,46 @@ const rules: KarabinerRules[] = [
         to_if_alone: [
           {
             key_code: "escape",
+          },
+        ],
+        type: "basic",
+      },
+      {
+        description: "Page down",
+        from: {
+          key_code: "d",
+          modifiers: {
+            mandatory: [
+              "left_command",
+              "left_control",
+              "left_shift",
+              "left_option",
+            ],
+          },
+        },
+        to: [
+          {
+            key_code: "page_down",
+          },
+        ],
+        type: "basic",
+      },
+      {
+        description: "Page up",
+        from: {
+          key_code: "u",
+          modifiers: {
+            mandatory: [
+              "left_command",
+              "left_control",
+              "left_shift",
+              "left_option",
+            ],
+          },
+        },
+        to: [
+          {
+            key_code: "page_up",
           },
         ],
         type: "basic",
@@ -46,160 +86,65 @@ const rules: KarabinerRules[] = [
     // o = "Open" applications
     o: {
       g: app("Google Chrome"),
-      c: app("Cron"),
       v: app("Visual Studio Code"),
-      d: app("Discord"),
-      s: app("Slack"),
-      e: app("Superhuman"),
       n: app("Notion"),
-      t: app("Terminal"),
+      // t: Terminal
+      t: app("Alacritty"),
       // Open todo list managed via *H*ypersonic
-      h: open(
-        "notion://www.notion.so/stellatehq/7b33b924746647499d906c55f89d5026"
-      ),
-      z: app("zoom.us"),
-      m: app("Mochi"),
+      // h: open(
+      //   "notion://www.notion.so/stellatehq/7b33b924746647499d906c55f89d5026"
+      // ),
+      s: app("Spotify"),
       f: app("Figma"),
-      r: app("Telegram"),
-      // "i"Message
-      i: app("Messages"),
-      p: app("Spotify"),
-      a: app("iA Presenter"),
-      w: open("https://web.whatsapp.com"),
-      l: open(
-        "raycast://extensions/stellate/mxstbr-commands/open-mxs-is-shortlink"
-      ),
+      a: app("Arc"),
+      // l: open(
+      //   "raycast://extensions/stellate/mxstbr-commands/open-mxs-is-shortlink"
+      // ),
     },
 
     // w = "Window" via rectangle.app
     w: {
-      semicolon: {
-        description: "Window: Hide",
-        to: [
-          {
-            key_code: "h",
-            modifiers: ["right_command"],
-          },
-        ],
-      },
-      y: {
-        description: "Window: First Third",
-        to: [
-          {
-            key_code: "left_arrow",
-            modifiers: ["right_option", "right_control"],
-          },
-        ],
-      },
-      k: {
-        description: "Window: Top Half",
-        to: [
-          {
-            key_code: "up_arrow",
-            modifiers: ["right_option", "right_command"],
-          },
-        ],
-      },
-      j: {
-        description: "Window: Bottom Half",
-        to: [
-          {
-            key_code: "down_arrow",
-            modifiers: ["right_option", "right_command"],
-          },
-        ],
-      },
-      o: {
-        description: "Window: Last Third",
-        to: [
-          {
-            key_code: "right_arrow",
-            modifiers: ["right_option", "right_control"],
-          },
-        ],
-      },
-      h: {
-        description: "Window: Left Half",
-        to: [
-          {
-            key_code: "left_arrow",
-            modifiers: ["right_option", "right_command"],
-          },
-        ],
-      },
-      l: {
-        description: "Window: Right Half",
-        to: [
-          {
-            key_code: "right_arrow",
-            modifiers: ["right_option", "right_command"],
-          },
-        ],
-      },
-      f: {
-        description: "Window: Full Screen",
-        to: [
-          {
-            key_code: "f",
-            modifiers: ["right_option", "right_command"],
-          },
-        ],
-      },
-      u: {
-        description: "Window: Previous Tab",
-        to: [
-          {
-            key_code: "tab",
-            modifiers: ["right_control", "right_shift"],
-          },
-        ],
-      },
-      i: {
-        description: "Window: Next Tab",
-        to: [
-          {
-            key_code: "tab",
-            modifiers: ["right_control"],
-          },
-        ],
-      },
-      n: {
-        description: "Window: Next Window",
-        to: [
-          {
-            key_code: "grave_accent_and_tilde",
-            modifiers: ["right_command"],
-          },
-        ],
-      },
-      b: {
-        description: "Window: Back",
-        to: [
-          {
-            key_code: "open_bracket",
-            modifiers: ["right_command"],
-          },
-        ],
-      },
-      // Note: No literal connection. Both f and n are already taken.
-      m: {
-        description: "Window: Forward",
-        to: [
-          {
-            key_code: "close_bracket",
-            modifiers: ["right_command"],
-          },
-        ],
-      },
-      d: {
-        description: "Window: Next display",
-        to: [
-          {
-            key_code: "right_arrow",
-            modifiers: ["right_control", "right_option", "right_command"],
-          },
-        ],
-      },
+      // Window navigation
+      y: command("Window: Navigate to Left", "yabai -m window --focus west"),
+      u: command("Window: Navigate to Down", "yabai -m window --focus south"),
+      i: command("Window: Navigate to Up", "yabai -m window --focus north"),
+      o: command("Window: Navigate to Right", "yabai -m window --focus east"),
+      // Window movements
+      h: command("Window: Move Left", "yabai -m window --warp west"),
+      j: command("Window: Move Down", "yabai -m window --warp south"),
+      k: command("Window: Move Up", "yabai -m window --warp north"),
+      l: command("Window: Move Right", "yabai -m window --warp east"),
+      // Window resizing
+      n: command(
+        "Window: Resize Left",
+        "yabai -m window --resize left:-50:0; \
+        yabai -m window --resize right:-50:0"
+      ),
+      m: command(
+        "Window: Resize Down",
+        "yabai -m window --resize bottom:0:50; \
+        yabai -m window --resize top:0:50"
+      ),
+      comma: command(
+        "Window: Resize Up",
+        "yabai -m window --resize top:0:-50; \
+        yabai -m window --resize bottom:0:-500"
+      ),
+      period: command(
+        "Window: Resize Right",
+        "yabai -m window --resize right:50:0; \
+        yabai -m window --resize left:50:0"
+      ),
+      t: command(
+        "Window: Toggle Float/Unfloat",
+        "yabai -m window --toggle float; \
+          yabai -m window --toggle border"
+      ),
+      f: command(
+        "Window: Full Screen",
+        "yabai -m window --toggle zoom-fullscreen"
+      ),
+      r: command("Window: Rotate", "yabai -m space --rotate 90"),
     },
 
     // s = "System"
@@ -263,21 +208,6 @@ const rules: KarabinerRules[] = [
           },
         ],
       },
-      // Turn on Elgato KeyLight
-      y: {
-        to: [
-          {
-            shell_command: `curl -H 'Content-Type: application/json' --request PUT --data '{ "numberOfLights": 1, "lights": [ { "on": 1, "brightness": 100, "temperature": 215 } ] }' http://192.168.8.84:9123/elgato/lights`,
-          },
-        ],
-      },
-      h: {
-        to: [
-          {
-            shell_command: `curl -H 'Content-Type: application/json' --request PUT --data '{ "numberOfLights": 1, "lights": [ { "on": 0, "brightness": 100, "temperature": 215 } ] }' http://192.168.8.84:9123/elgato/lights`,
-          },
-        ],
-      },
     },
 
     // v = "moVe" which isn't "m" because we want it to be on the left hand
@@ -295,17 +225,17 @@ const rules: KarabinerRules[] = [
       l: {
         to: [{ key_code: "right_arrow" }],
       },
-      // Magicmove via homerow.app
-      m: {
-        to: [{ key_code: "f", modifiers: ["right_control"] }],
-      },
-      // Scroll mode via homerow.app
-      s: {
-        to: [{ key_code: "j", modifiers: ["right_control"] }],
-      },
-      d: {
-        to: [{ key_code: "d", modifiers: ["right_shift", "right_command"] }],
-      },
+      // // Magicmove via homerow.app
+      // m: {
+      //   to: [{ key_code: "f", modifiers: ["right_control"] }],
+      // },
+      // // Scroll mode via homerow.app
+      // s: {
+      //   to: [{ key_code: "j", modifiers: ["right_control"] }],
+      // },
+      // d: {
+      //   to: [{ key_code: "d", modifiers: ["right_shift", "right_command"] }],
+      // },
       u: {
         to: [{ key_code: "page_down" }],
       },
@@ -329,21 +259,9 @@ const rules: KarabinerRules[] = [
 
     // r = "Raycast"
     r: {
-      l: open(
-        "raycast://extensions/stellate/mxstbr-commands/create-mxs-is-shortlink"
-      ),
       e: open("raycast://extensions/raycast/emoji/search-emoji"),
-      c: open("raycast://extensions/raycast/raycast/confetti"),
-      a: open("raycast://extensions/raycast/raycast-ai/ai-chat"),
-      s: open("raycast://extensions/peduarte/silent-mention/index"),
       h: open(
         "raycast://extensions/raycast/clipboard-history/clipboard-history"
-      ),
-      1: open(
-        "raycast://extensions/VladCuciureanu/toothpick/connect-favorite-device-1"
-      ),
-      2: open(
-        "raycast://extensions/VladCuciureanu/toothpick/connect-favorite-device-2"
       ),
     },
   }),
@@ -354,7 +272,7 @@ fs.writeFileSync(
   JSON.stringify(
     {
       global: {
-        show_in_menu_bar: false,
+        show_in_menu_bar: true,
       },
       profiles: [
         {
